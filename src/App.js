90786c31b2f3866e1665c73332ad4b0a6568c621
RikkamalleKamalnath <rikkamallekamalnath@gmail.com>
2024-04-11 12:07:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => (
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
-          <Route exact path="/contact" component={Content} />
+          <Route exact path="/contact" component={Contact} />
           <Route component={NotFound} />
         </Switch>
       </div>
